d29e0e5b32ffa3f90731c264b04d3d948bbf1d94
minhdat1147 <minhdat072000@gmail.com>
2024-05-24 14:14:09 +0700

    Spring API with @PathVariable

--- src/main/java/com/springboot/app/controller/StudentController.java
+++ src/main/java/com/springboot/app/controller/StudentController.java
@@ -2,12 +2,16 @@ package com.springboot.app.controller;
 
 import com.springboot.app.entity.StudentEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
-    @GetMapping("/student")
-    public StudentEntity getStudent(){
-        return new StudentEntity("Thai Minh", "Dat");
+    @GetMapping("/student/{firstName}/{lastName}")
+    public StudentEntity getStudent(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName){
+        return new StudentEntity(firstName, lastName);
     }
 }
